d8f7df5c7f9fa05b8577afd70533da6b8614f458
ABid A.Sobhan <abida.sobhan@ABids-iMac.local>
2020-09-10 01:44:48 -0700

    Navigation Side Bar Swift

--- Navigation Drawer/ViewController.swift
+++ Navigation Drawer/ViewController.swift
@@ -8,13 +8,84 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDelegate,UITableViewDataSource{
+   
+  var arraydata = ["Home","Email","Shared Device","Logout"]
+    var arrayimg = [#imageLiteral(resourceName: "name"),#imageLiteral(resourceName: "email"),#imageLiteral(resourceName: "mobile"),#imageLiteral(resourceName: "password")]
+    
+    
+    
+    
+    
+    
 
+    @IBOutlet weak var sideview: UIView!
+    
+    
+    @IBOutlet weak var sidebar: UITableView!
+    
+    
+    var isSideviewOpen:  Bool = false
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+           return arraydata.count
+    }
+    
+  
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:TableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell") as! TableViewCell
+        cell.img.image = arrayimg[indexPath.row]
+        cell.lbl.text = arraydata[indexPath.row]
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        sideview.isHidden = true
+        UIColor.groupTableViewBackground
+        isSideviewOpen = false
+        
     }
 
 
+    @IBAction func sidebarbutton(_ sender: Any) {
+        
+        sidebar.isHidden = false
+        sideview.isHidden = false
+        self.view.bringSubviewToFront(sideview)
+        
+        if !isSideviewOpen {
+            isSideviewOpen = true
+            sideview.frame = CGRect.init(x: 0, y: 88, width: 0, height: 808)
+            sidebar.frame = CGRect.init(x: 0, y: 0, width: 0, height: 808)
+            UIView.setAnimationDuration(0.5)
+            UIView.setAnimationDelegate(self)
+            UIView.beginAnimations("TableAnimation", context: nil)
+        
+            sideview.frame = CGRect.init(x: 0, y: 88, width: 240, height: 808)
+            sidebar.frame = CGRect.init(x: 0, y: 0, width: 240, height: 808)
+            UIView.commitAnimations()
+            
+        }else{
+            
+            sidebar.isHidden = true
+            sideview.isHidden = true
+            isSideviewOpen = false
+            sideview.frame = CGRect.init(x: 0, y: 88, width: 0, height: 808)
+            sidebar.frame = CGRect.init(x: 0, y: 0, width: 0, height: 808)
+            UIView.setAnimationDuration(0.5)
+            UIView.setAnimationDelegate(self)
+            UIView.beginAnimations("TableAnimation", context: nil)
+            
+            sideview.frame = CGRect.init(x: 0, y: 88, width: 240, height: 808)
+            sidebar.frame = CGRect.init(x: 0, y: 0, width: 240, height: 808)
+            UIView.commitAnimations()
+            
+        }
+        
+        
+    }
 }
 
